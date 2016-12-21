735d96695aaff80ddd3c2049270bb2e12b697388
Daniel Słaby <ds@colortv.com>
2016-12-21 18:13:00 +0100

    Added immutable.js

--- src/ducks/Tumblr.js
+++ src/ducks/Tumblr.js
@@ -1,23 +1,21 @@
 import { createAction, handleActions } from 'redux-actions'
-
+import { Map, fromJS } from 'immutable'
 // ACTIONS
 export const GET_USER = createAction('DEMO/TUMBLR/START_GET_USER')
 export const END_GET_USER = createAction('DEMO/TUMBLR/END_GET_USER')
 export const END_GET_USER_ERROR = createAction('DEMO/TUMBLR/END_GET_USER_ERROR')
 // REDUCER
-const defaultState = {}
+const defaultState = new Map({})
 
 const reducer = handleActions({
-  [GET_USER().type]: function (state, action) {
-    console.log('ACTION', action)
+  [GET_USER().type]: function (state, { payload, type, error }) {
     return state
   },
-  [END_GET_USER().type]: function (state, action) {
-    console.log('ACTION', action)
-    return state
+  [END_GET_USER().type]: function (state, { payload, type, error }) {
+    const user = fromJS(payload.response.blog)
+    return state.set('user', user)
   },
-  [END_GET_USER_ERROR().type]: function (state, action) {
-    console.log('ACTION', action)
+  [END_GET_USER_ERROR().type]: function (state, { payload, type, error }) {
     return state
   },
 }, defaultState)
