4f7e01d2bb156118ebc5c30709c610324a6a51bb
Daniel Słaby <pufek93@gmail.com>
2017-02-05 19:50:32 +0100

    added video sidebar background with looped video

--- src/components/dumb/Navigation/SideBar/SideBar.dumb.js
+++ src/components/dumb/Navigation/SideBar/SideBar.dumb.js
@@ -6,32 +6,35 @@ import bemClassname from 'bem-classname'
 import MenuItem from './MenuItem'
 
 class SideBar extends React.PureComponent {
-  constructor () {
-    super()
-    this.className = bemClassname.bind(null, 'SideBar')
-  }
+    constructor() {
+        super()
+        this.className = bemClassname.bind(null, 'SideBar')
+    }
 
-  mapItemsToElements (items, index) {
-    const result = items.map((item) => {
-      return (
-        <MenuItem
-          key={item.get('path')}
-          name={item.get('name')}
-          path={item.get('path')}
-        />)
-    })
+    mapItemsToElements(items, index) {
+        const result = items.map((item) => {
+            return (
+                <MenuItem
+                    key={item.get('path')}
+                    name={item.get('name')}
+                    path={item.get('path')}
+                />)
+        })
 
-    return result
-  }
+        return result
+    }
 
-  render () {
-    const items = this.mapItemsToElements(this.props.items)
-    return (
-      <div className={this.className()}>
-        {items}
-      </div>
-    )
-  }
+    render() {
+        const items = this.mapItemsToElements(this.props.items)
+        return (
+            <div className={this.className()}>
+                {items}
+                <video className={this.className('video')} preload autoPlay loop muted>
+                    <source src="./../resources/a.mp4"/>
+                </video>
+            </div>
+        )
+    }
 }
 
 export default SideBar
